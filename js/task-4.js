537bb60377b4c0fec28309156ff832e8640ef8af
IvanYuryk <143449289+IvanYuryk@users.noreply.github.com>
2023-12-14 20:50:25 +0100

    v4

--- js/task-4.js
+++ js/task-4.js
@@ -1,31 +1,47 @@
-// function getShippingCost(country) {
-//     let shippingCost;
+const getTotalBalanceByGender = (users, gender) =>
+    users
+      .filter((user) => user.gender === gender) 
+      .reduce((totalBalance, user) => totalBalance + user.balance, 0);
+      
+      const eachUsers = [
+    {
+      name: "Moore Hensley",
+      gender: "male",
+      balance: 2811,
+    },
+    {
+      name: "Sharlene Bush",
+      gender: "female",
+      balance: 3821,
+    },
+    {
+      name: "Ross Vazquez",
+      gender: "male",
+      balance: 3793,
+    },
+    {
+      name: "Elma Head",
+      gender: "female",
+      balance: 2278,
+    },
+    {
+      name: "Carey Barr",
+      gender: "male",
+      balance: 3951,
+    },
+    {
+      name: "Blackburn Dotson",
+      gender: "male",
+      balance: 1498,
+    },
+    {
+      name: "Sheree Anthony",
+      gender: "female",
+      balance: 2764,
+    },
+  ];
   
-//     switch (country.toLowerCase()) {
-//       case "china":
-//         shippingCost = 100;
-//         break;
-//       case "chile":
-//         shippingCost = 250;
-//         break;
-//       case "australia":
-//         shippingCost = 170;
-//         break;
-//       case "jamaica":
-//         shippingCost = 120;
-//         break;
-//       default:
-//         return "Sorry, there is no delivery to your country";
-//     }
   
-//     return `Shipping to ${country} will cost ${shippingCost} credits`;
-//   }
-
-
-
-// console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
-// console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
-// console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
-// console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
-// console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
-// console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+  console.log(getTotalBalanceByGender(eachUsers, "male")); // 12053
+  console.log(getTotalBalanceByGender(eachUsers, "female")); // 8863
+  
